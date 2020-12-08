34418801ce76ee74b6a3ffcd25f97a40cfd618f2
Karan Dehghani <karan@codedoor.org>
2020-12-08 08:32:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request
+from random import choice
+
+web_site = Flask(__name__)
+
+number_list = [
+	100, 101, 200, 201, 202, 204, 206, 207, 300, 301, 302, 303, 304, 305, 307, 400, 401, 402, 403, 404, 405, 406, 408, 409, 410, 411, 412, 413, 414, 415,
+	416, 417, 418, 421, 422, 423, 424, 425, 426,
+	429, 431, 444, 450, 451, 500, 502, 503, 504, 506, 507, 508, 509, 510, 511, 599
+]
+
+@web_site.route('/')
+def index():
+	return render_template('index.html')
+
+@web_site.route('/rfactor.html')
+def rfactor():
+  return render_template('rfactor.html')
+
+@web_site.route('/bio.html')
+def bio():
+  return render_template('bio.html')
+
+@web_site.route('/asense.html')
+def asense():
+  return render_template('asense.html')
+
+@web_site.route('/books.html')
+def books():
+  return render_template('books.html')
+
+@web_site.route('/quotes.html')
+def quotes():
+  return render_template('quotes.html')
+
+@web_site.route('/drawings.html')
+def drawings():
+  return render_template('drawings.html')
+
+@web_site.route('/essays.html')
+def essays():
+  return render_template('essays.html')  
+
+
+@web_site.route('/user/', defaults={'username': None})
+@web_site.route('/user/<username>')
+def generate_user(username):
+	if not username:
+		username = request.args.get('username')
+
+	if not username:
+		return 'Sorry error something, malformed request.'
+
+	return render_template('personal_user.html', user=username)
+
+@web_site.route('/page')
+def random_page():
+  return render_template('page.html', code=choice(number_list))
+
+
+
+
+web_site.run(host='0.0.0.0', port=8080)
